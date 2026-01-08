5a0d6265ce7242d50c630aa75e57725fe931bb20
klimentsii <klimparh@gmail.com>
2026-01-08 20:17:16 +0300

    changed routes saving

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
-import { GAMES, Game } from './games.constants';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { GAMES } from './games.constants';
 
 @Component({
   selector: 'app-root',
@@ -14,34 +13,11 @@ export class App {
   protected readonly title = signal('nickspin.app');
   protected readonly games = GAMES;
 
-  protected readonly currentUrl = signal(this.router.url);
-
-  constructor() {
-    if (this.router.url === '/') {
-      this.router.navigate(['/roblox']);
-    }
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this.currentUrl.set(event.url);
-      });
-  }
-
-  protected readonly activeGameId = computed(() => {
-    const url = this.currentUrl();
-    const match = url.match(/\/([^\/]+)/);
-    return match ? match[1] : 'roblox';
-  });
-
-  protected readonly currentGame = computed(() => {
-    return GAMES.find(game => game.id === this.activeGameId()) || GAMES[0];
-  });
-
-  protected isActive(gameId: string): boolean {
-    return this.activeGameId() === gameId;
-  }
-
   protected selectGame(gameId: string): void {
     this.router.navigate(['/', gameId]);
+  }
+
+  protected isActive(gameId: string): boolean {
+    return this.router.url.includes(gameId);
   }
 }
